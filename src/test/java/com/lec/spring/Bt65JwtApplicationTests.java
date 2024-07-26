c7f552e02ea9263af23e0ae7a9e8b10aaa97dfc9
yehyeonn <yehyeon99@naver.com>
2024-07-26 14:28:07 +0900

    JWT 연습

---
+++ src/test/java/com/lec/spring/Bt65JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt65JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
